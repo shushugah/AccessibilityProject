1263e34eec4408b0b9c650c31757a31be04df1d5
Yonatan Miller <brombacher1@gmail.com>
2015-05-22 18:01:01 -0400

    changed location of bower_components to views folder, modified indexjs and corrected gitignore file

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-var express = require('express');
-var path = require('path');
-
-var app = express();
+var connect = require("connect");
+var serveStatic = require('serve-static'); 
+var app = connect();
 
 var PORT = (process.env.PORT || 3000); 
 
-app.get('/', function(request, response) {
-    response.sendFile(path.join(__dirname, '/views/static_pages', 'index.html'));
-});
+
+app.use(serveStatic("views/",{index: ["static_pages/index.html"]}))
+
 
 app.listen(PORT);
 console.log("Project Accessibility is running on port " + PORT);
